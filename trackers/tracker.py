fc4cf833a3b76f7d4b473dbf55ddc9a684bcc898
sbhat72 <sbhat72@uwo.ca>
2026-01-05 16:01:39 -0500

    Started Ellipses Drawing

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -7,6 +7,9 @@ import pandas as pd
 import cv2
 import sys 
 
+sys.path.append('../')
+from utils import get_center_of_bbox, get_bbox_width
+
 class Tracker:
     def __init__(self, model_path):
        self.model = YOLO(model_path)
@@ -19,15 +22,22 @@ class Tracker:
        for i in range(0,len(frames), batch_size):
            detections_batch = self.model.predict(frames[i:i+batch_size],conf=0.1)
            detections += detections_batch
-           break
+           
 
        return detections
 
-    def get_object_tracks(self,frames):
+    def get_object_tracks(self,frames,read_from_stub=False,stub_path=None):
+
+        if read_from_stub == True and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path,'rb') as f:
+                tracks = pickle.load(f)
+            
+            return tracks
+
         detections = self.detect_frames(frames)
 
         tracks = {
-            "players":[],
+            "players":[], 
             "referees":[],
             "ball":[]
         }
@@ -62,7 +72,7 @@ class Tracker:
                     tracks["players"][frame_num][track_id] = {"bbox":bbox}
 
                 if cls_id == cls_names_inv['referee']:
-                    tracks["referee"][frame_num][track_id] = {"bbox":bbox}
+                    tracks["referees"][frame_num][track_id] = {"bbox":bbox}
 
                 if cls_id == cls_names_inv['ball']:
                     tracks["ball"][frame_num][track_id] = {"bbox":bbox}
@@ -74,10 +84,42 @@ class Tracker:
                 if cls_id == cls_names_inv['ball']:
                     tracks["ball"][frame_num][1] = {"bbox":bbox}
 
-
+        if stub_path is not None:
+            with open(stub_path,'wb') as f:
+                pickle.dump(tracks,f)
         
 
         return tracks 
 
-            
 
+    def draw_ellipse(self,frame,bbox,color,track_id=None):
+        y2 = int(bbox[3])
+        x_center, _ = get_center_of_bbox(bbox)
+        width = get_bbox_width(bbox)
+
+        cv2.ellipse(frame, 
+                    center=(x_center, y2),
+                    axes=(int(width), int(0.35*width)),  
+                    angle=0.0,
+                    startAngle=45,  
+                    endAngle=235,    
+                    color=color,
+                    thickness=2,
+                    lineType=cv2.LINE_4)
+        return frame
+
+
+    def draw_annotations(self,video_frames,tracks):
+        output_video_frames = []
+        for frame_num,frame in enumerate(video_frames):
+            frame = frame.copy()
+
+            player_dict = tracks["players"][frame_num]
+            ball_dict = tracks["ball"][frame_num]
+            referee_dict = tracks["referees"][frame_num]
+
+            #Draw Players 
+            for track_id,player in player_dict.items():
+                frame = self.draw_ellipse(frame, player["bbox"], (0,0,255), track_id)
+            output_video_frames.append(frame)
+        return output_video_frames
