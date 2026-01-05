b70245dec8f92e62409015c2003ab636758552af
sbhat72 <sbhat72@uwo.ca>
2026-01-05 16:02:45 -0500

    Ellipses drawings included in Frames

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from trackers import Tracker
 def main():
     #Read Video
     video_frames = read_video('08fd33_4.mp4')
- 
-    #Save Video
-    save_videos(video_frames,'output_videos/output_video.avi')
     
     #Initialize Tracker 
     tracker = Tracker('models/best.pt')
 
-    tracks = tracker.get_object_tracks(video_frames)
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=True,
+                                       stub_path='stubs/track_stubs.pkl'  
+    )
+
+    #Draw output 
+    draw_output_video_frames = tracker.draw_annotations(video_frames,tracks)
     
+    #Save Video
+    save_videos(draw_output_video_frames,'output_videos/output_video.avi')
+
+
 if __name__ == '__main__':
     main()
