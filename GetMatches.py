1561a050b017f3d2066f1572476923e667e43f58
sbhat72 <sbhat72@uwo.ca>
2026-01-08 09:34:52 -0500

    YOLO Prediction Model

---
+++ GetMatches.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+model = YOLO('models/best.pt')
+
+
+results = model.predict('08fd33_4.mp4', save=True)
+
+
+print(results[0])
+print('========================')
+for box in results[0].boxes:
+    print(box)
